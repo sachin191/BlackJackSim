a709414971a4d76cbcdd3f162864591cfaa9934a
Sachin Sane <sachin191vex@gmail.com>
2015-09-20 19:36:51 -0700

    clean already dealt card logic

--- bjs.py
+++ bjs.py
@@ -7,13 +7,16 @@ gbjs_cards_dealt_list=[]
 def is_dealt_card(num):
     global gbjs_cards_dealt_list
     for i in range(len(gbjs_cards_dealt_list)):
-        if ( num == gbjs_card_dealth_list[i] ): 
+        print gbjs_cards_dealt_list
+        if ( num == gbjs_cards_dealt_list[i] ): 
           return 1
+
     return 0
 
 def get_next_card():
     global gbjs_num_decks
     global gbjs_cards_dealt_num
+    global gbjs_cards_dealt_list
 
     num_decks = gbjs_num_decks
     max_num = num_decks*52
@@ -24,6 +27,7 @@ def get_next_card():
     while (is_dealt_card(num) == 1):
 	    num=random.randint(1,max_num)
 
+    gbjs_cards_dealt_list.append(num)
     gbjs_cards_dealt_num += 1 
     return num
 
@@ -32,13 +36,15 @@ def card_num2str(num):
     card=num%52
     suit=card/13
     val=card%13
+    if ( val == 0 ):
+        val = 13
     return deck, suit, val
 
 def start_game():
-    for i in range(0,10):
+    for i in range(0,55):
         num=get_next_card();
         deck, suit, val = card_num2str(num)
-        print deck, suit, val
+        print i, num, deck, suit, val
 
 def main():
     start_game()
